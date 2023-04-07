045fa28cd32a8a5b378e9822e1618ab218d273a9
Karn Jaroenporn <karnjj@hotmail.com>
2023-04-07 23:52:13 +0700

    feat: init backend socket io (#1)

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,6 +1,10 @@
 import express, { Express, Request, Response } from 'express'
+import http from 'http'
+import { Server } from 'socket.io'
 
 const app: Express = express()
+const server = http.createServer(app)
+const io = new Server(server)
 
 const port = 8000
 
@@ -10,4 +14,11 @@ app.get('/', (req: Request, res: Response) => {
   })
 })
 
-app.listen(port, () => console.log(`Application is running on port ${port}`))
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
+
+server.listen(port, () => console.log(`Application is running on port ${port}`))
